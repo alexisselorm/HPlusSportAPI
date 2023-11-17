6c77602b9b3426a43650458bdb350ab1a52194a8
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-11-17 14:57:14 +0200

    Searching items

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -28,6 +28,17 @@ namespace HPlusSport.API.Controllers;
             {
                 products = products.Where(p => p.Price <= queryParameters.MaxPrice);
             }
+
+            if (!string.IsNullOrEmpty(queryParameters.Sku))
+            {
+                products = products.Where(p=>p.Sku == queryParameters.Sku);
+            }
+            if (!string.IsNullOrEmpty(queryParameters.Name))
+            {
+                products = products.Where(p=>p.Name.ToLower().Contains(queryParameters.Name.ToLower()
+                ));
+            }
+            
             products = products.Skip(queryParameters.Size * (queryParameters.Page - 1))
                 .Take(queryParameters.Size);
             
--- Models/ProductQueryParameters.cs
+++ Models/ProductQueryParameters.cs
@@ -2,6 +2,8 @@
 
 public class ProductQueryParameters : QueryParameters
 {
+    public string? Name { get; set; } = string.Empty;
+    public string? Sku { get; set; } = string.Empty;
     public decimal? MinPrice { get; set; }
     public decimal? MaxPrice { get; set; }
     
